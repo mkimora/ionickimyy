110a1c5cecc4dbcfe7b7e6140ccbdd5ed25f6427
kimora <guissendiaye24@gmail.com>
2019-10-22 17:32:55 +0000

    kiki was here

--- src/app/envoiretrait/envoiretrait.page.ts
+++ src/app/envoiretrait/envoiretrait.page.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnvoiretraitPage implements OnInit {
 
+  public buttonClicked: boolean = false; //Whatever you want to initialise it as 
+  public butonClicked: boolean = false;
+
+  //masquer l' envoi
+  public onButtonClick() {
+    this.butonClicked = !this.butonClicked;
+    this.buttonClicked = this.buttonClicked;
+  }
+  //masquer le retrait
+  public offButtonClick() {
+    this.buttonClicked = !this.buttonClicked;
+    this.butonClicked = this.butonClicked;
+  }
+
   constructor() { }
 
   ngOnInit() {
